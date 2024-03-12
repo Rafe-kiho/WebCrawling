f5f88d24adaac6380dc11d0c4060cca2728d22c1
Rafe-kiho <64768348+Rafe-kiho@users.noreply.github.com>
2024-03-13 08:48:16 +0900

    crawling sample

--- webcrawling.py
+++ webcrawling.py
@@ -11,6 +11,8 @@ driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), opti
 driver.get(url='https://flight.naver.com/')
 time.sleep(3)
 
+driver.find_element(By.XPATH, '//*[@id="__next"]/div/div[1]/div[4]/div/div/div[1]/button[2]').click()
+
 driver.find_element(By.XPATH, '//*[@id="__next"]/div/div[1]/div[4]/div/div/div[2]/div[1]/button[2]').click()
 time.sleep(1)
 
@@ -20,8 +22,36 @@ time.sleep(1)
 driver.find_element(By.XPATH, '//i[contains(text(), "간사이국제공항")]').click()
 time.sleep(1)
 
+driver.find_element(By.XPATH, '//*[@id="__next"]/div/div[1]/div[4]/div/div/div[2]/div[2]/button[1]').click()
+time.sleep(1)
+
+driver.find_elements(By.XPATH, '//b[text() = "16"]')[0].click()
+time.sleep(1)
+
+driver.find_element(By.XPATH, '//*[@id="__next"]/div/div[1]/div[4]/div/div/div[2]/button').click()
+time.sleep(10)
+
+
+
 # driver.find_element(By.XPATH, '').click()
 # time.sleep(1)
+
+# 전체 : indivisual_IndivisualItem__3co62 result
+# 항공사 : airline_Airlines__8QpMj
+# 출발 : route_Route__2UInh
+# 가격 : item_defaultSummary__3hCaV
+
+results = driver.find_elements(By.CLASS_NAME, 'indivisual_IndivisualItem__3co62.result')
+for result in results:
+    try:
+        airline = result.find_element(By.CLASS_NAME, 'airline_Airlines__8QpMj').text
+        startTime = result.find_element(By.CLASS_NAME, 'route_Route__2UInh').text
+        price = result.find_element(By.CLASS_NAME, 'item_defaultSummary__3hCaV').text
+        print(f'항공사 : {airline}')
+        print(f'출발시간 : {startTime}')
+        print(f'가격 : {price}')
+    except:
+        pass
 
 
 
